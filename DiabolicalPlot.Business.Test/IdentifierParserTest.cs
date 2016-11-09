e9ba8316e0ea1afbe469b921c77659ad3973a330
Steven Doggart <stevendoggart@gmail.com>
2016-11-08 22:06:07 -0500

    Add IdentifierParser

---
+++ DiabolicalPlot.Business.Test/IdentifierParserTest.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Collections;
+
+
+namespace DiabolicalPlot.Business.Test
+{
+    [TestClass]
+    public class IdentifierParserTest
+    {
+        [TestMethod]
+        public void Parse_null_returns_empty_list()
+        {
+            var parser = new IdentifierParser();
+            var result = parser.Parse(null);
+            Assert.IsNotNull(result);
+            Assert.IsFalse(result.Any());
+
+        }
+
+
+        [TestMethod]
+        public void Parse_empty_string_returns_empty_list()
+        {
+            var parser = new IdentifierParser();
+            var result = parser.Parse("");
+            Assert.IsNotNull(result);
+            Assert.IsFalse(result.Any());
+        }
+
+
+        [TestMethod]
+        public void Parse_word()
+        {
+            var parser = new IdentifierParser();
+            var result = parser.Parse("word").ToArray();
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.Count());
+            Assert.AreEqual(IdentifierPartTypeEnum.Word, result[0].Type);
+            Assert.AreEqual("word", result[0].Value);
+        }
+
+
+        [TestMethod]
+        public void Parse_oneTwoTHREEFour5Six7eight_9_ten()
+        {
+            var parser = new IdentifierParser();
+            var result = parser.Parse("oneTwoTHREEFour5Six7eight_9__ten").ToArray();
+            Assert.IsNotNull(result);
+            Assert.AreEqual(12, result.Count());
+
+            Assert.AreEqual(IdentifierPartTypeEnum.Word, result[0].Type);
+            Assert.AreEqual("one", result[0].Value);
+
+            Assert.AreEqual(IdentifierPartTypeEnum.Word, result[1].Type);
+            Assert.AreEqual("Two", result[1].Value);
+
+            Assert.AreEqual(IdentifierPartTypeEnum.Word, result[2].Type);
+            Assert.AreEqual("THREE", result[2].Value);
+
+            Assert.AreEqual(IdentifierPartTypeEnum.Word, result[3].Type);
+            Assert.AreEqual("Four", result[3].Value);
+
+            Assert.AreEqual(IdentifierPartTypeEnum.Word, result[4].Type);
+            Assert.AreEqual("5", result[4].Value);
+
+            Assert.AreEqual(IdentifierPartTypeEnum.Word, result[5].Type);
+            Assert.AreEqual("Six", result[5].Value);
+
+            Assert.AreEqual(IdentifierPartTypeEnum.Word, result[6].Type);
+            Assert.AreEqual("7", result[6].Value);
+
+            Assert.AreEqual(IdentifierPartTypeEnum.Word, result[7].Type);
+            Assert.AreEqual("eight", result[7].Value);
+
+            Assert.AreEqual(IdentifierPartTypeEnum.Other, result[8].Type);
+            Assert.AreEqual("_", result[8].Value);
+
+            Assert.AreEqual(IdentifierPartTypeEnum.Word, result[9].Type);
+            Assert.AreEqual("9", result[9].Value);
+
+            Assert.AreEqual(IdentifierPartTypeEnum.Other, result[10].Type);
+            Assert.AreEqual("__", result[10].Value);
+
+            Assert.AreEqual(IdentifierPartTypeEnum.Word, result[11].Type);
+            Assert.AreEqual("ten", result[11].Value);
+        }
+    }
+}
---
+++ DiabolicalPlot.Business/IdentifierParser.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+
+namespace DiabolicalPlot.Business
+{
+    public class IdentifierParser : IIdentifierParser
+    {
+        public IEnumerable<IdentifierPart> Parse(string identifier)
+        {
+            const string pattern = @"
+                (?<=\p{Ll})(?=[\p{Lu}])           |
+                (?<=[^_])(?=_)                    |
+                (?<=_)(?=[^_])                    |
+                (?<=\d)(?=\D)                     |
+                (?<=\D)(?=\d)                     |
+                (?<=[\p{Lu}])(?=[\p{Lu}][\p{Ll}])
+                ";
+            string[] parts;
+            if (identifier != null)
+                parts = Regex.Split(identifier, pattern, RegexOptions.IgnorePatternWhitespace);
+            else
+                parts = new string[] { };
+            return parts
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                .Select((part, index) => evaluatePart(part, parts, index));
+        }
+
+
+        private IdentifierPart evaluatePart(string part, string[] parts, int index)
+        {
+            IdentifierPartTypeEnum? type = null;
+            if ((index == 0) && (part.Length == 1) && ("IT".Contains(part)))
+                type = IdentifierPartTypeEnum.Other;
+            if ((!type.HasValue) && Regex.IsMatch(part, @"^[^_]+$"))
+                type = IdentifierPartTypeEnum.Word;
+            return new IdentifierPart(type.HasValue ? type.Value : IdentifierPartTypeEnum.Other, part);
+        }
+    }
+
+
+    public interface IIdentifierParser
+    {
+        IEnumerable<IdentifierPart> Parse(string identifier);
+    }
+
+
+    public class IdentifierPart
+    {
+        public IdentifierPart(IdentifierPartTypeEnum type, string value)
+        {
+            Type = type;
+            Value = value;
+        }
+
+        public IdentifierPartTypeEnum Type { get; }
+        public string Value { get; }
+    }
+
+
+    public enum IdentifierPartTypeEnum
+    {
+        Other,
+        Word
+    }
+}
